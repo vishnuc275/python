d4758ab2bea305d28e55ba0321d5eacb764d166e
VISHNU CHANDRAN <vishnuc275@gmail.com>
2020-12-20 17:38:48 +0530

    Add files via upload

---
+++ vowelcons.py
@@ -0,0 +1,7 @@
+x =(input("Enter a character: "))
+
+if(x=='A' or x=='a' or x=='E' or x=='e' or x=='I'
+ or x=='i' or x=='O' or x=='o' or x=='U' or x=='u'):
+    print(x, "is a Vowel")
+else:
+    print(x, "is a Consonant")
